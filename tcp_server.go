1a1f8bf1fe6c2296a04cca9744782408c5388728
Viktor <vjkalhovd@uia.no>
2023-04-21 08:42:26 +0000

    kryptering og fil

--- tcp_server.go
+++ tcp_server.go
@@ -41,6 +41,7 @@ func main() {
 					dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
 					log.Println("Dekrypter melding: ", string(dekryptertMelding))
 					switch msg := string(dekryptertMelding); msg {
+                                        //log.Println(msg)
   				        case "ping":
 						_, err = c.Write([]byte("pong"))
 					default:
